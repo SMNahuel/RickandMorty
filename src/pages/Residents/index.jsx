2049bc7927008250e47118139ce6dc9c120950b2
SMNahuel <nahuelsan96@gmail.com>
2021-12-03 11:35:17 -0300

    feat : filter residents

--- src/pages/Residents/index.jsx
+++ src/pages/Residents/index.jsx
@@ -2,29 +2,65 @@ import Card from 'components/Card';
 import { useEffect, useState } from 'react';
 /* -------------------------------------------------Redux */
 import { connect } from "react-redux";
+import Filter from 'components/Filter';
+import { filterGender, filterGenderStatus, filterStatus } from '../../utils/page';
 
-const Residents = ({ residents }) => {
-    const [state, setState ] = useState(residents)
-    useEffect(()=> {
-        setState(residents)
-    },[residents])
+const Residents = ({ residents, gender, status, filter }) => {
+    const [state, setState] = useState(residents)
+
+    useEffect(() => {
+        const handleFilter = () => {
+            let newContainer = []
+            if (filter) {
+                if (gender !== null && status !== null) {
+                    newContainer = filterGenderStatus(residents, gender, status)
+                } else if (gender) {
+                    newContainer = filterGender(residents, gender)
+                } else if (status) {
+                    newContainer = filterStatus(residents, status)
+                }
+            }
+            setState(newContainer)
+            newContainer = []
+        }
+        handleFilter()
+    }, [gender, status,filter,residents])
+
+
+
     return (
         <div>
+            <Filter />
             <div className="cardContainer">
                 {
-                    state.map((character) => {
+                    filter && state.map((character) => {
                         return (
                             <Card character={character} key={character.id} />
                         )
                     })
                 }
             </div>
+
+            <div className="cardContainer">
+                {
+                    !filter && residents.map((character) => {
+                        return (
+                            <Card character={character} key={character.id} />
+                        )
+                    })
+                }
+            </div>
+
+
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    residents: state.residents
+    residents: state.residents,
+    gender: state.gender,
+    status: state.status,
+    filter: state.filter
 })
 
 export default connect(mapStateToProps)(Residents);
--- src/utils/page.js
+++ src/utils/page.js
@@ -20,11 +20,33 @@ export const firstRequest = async (dispatch) => {
 export const requestResidents = async (dispatch, residents, navigate) => {
   const result = [];
   residents.map(async (resident) => {
-    await axios.get(resident).then(({data}) => {
-      result.push(data)
+    await axios.get(resident).then(({ data }) => {
+      result.push(data);
     });
   });
   dispatch(REQUEST_TO_RESIDENTS(result));
-  setTimeout(function(){ navigate("/locations/residents"); }, 3000);
-  
+  setTimeout(function () {
+    navigate("/locations/residents");
+  }, 3000);
+};
+
+export const filterGender = (residents, gender) => {
+  let newContainer = residents.filter((resident) => {
+   return resident.gender.toLowerCase() === gender;
+  });
+  return newContainer;
+};
+
+export const filterStatus = (residents, status) => {
+  let newContainer = residents.filter((resident) => {
+   return resident.status.toLowerCase() === status;
+  });
+  return newContainer;
+};
+
+export const filterGenderStatus = (residents, gender, status) => {
+  let newContainer = residents.filter((resident) => {
+    return resident.status.toLowerCase() === status && resident.gender.toLowerCase() === gender;
+   });
+   return newContainer;
 };
